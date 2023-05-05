1c08de59899052ea1f78a357e7defa57857898d0
Javich23 <jchavezchaidez@appdev.yearup.org>
2023-05-04 21:27:07 -0400

    "Finished Project, added comments and readme file"

---
+++ src/main/java/com/ledger/app/Reports.java
@@ -0,0 +1,111 @@
+package com.ledger.app;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+import static com.ledger.app.Ledger.transactions;
+
+public class Reports {
+    static Scanner consoleInput = new Scanner(System.in);
+    //report menu using while loop and switch statement for user input
+    public static void showReports() {
+        boolean done = false;
+        while (!done) {
+            System.out.println("\nSELECT REPORT TYPE\n");
+            System.out.println("""
+                    [1] - MONTH TO DATE
+                    [2] - PREVIOUS MONTH
+                    [3] - YEAR TO DATE
+                    [4] - PREVIOUS YEAR
+                    [5] - SEARCH BY VENDOR
+                    [0] - BACK
+                    """);
+            System.out.print("YOUR CHOICE: ");
+            int choice = consoleInput.nextInt();
+            consoleInput.nextLine();
+            switch (choice) {
+                case 1 -> monthToDate();
+                case 2 -> previousMonth();
+                case 3 -> yearToDate();
+                case 4 -> previousYear();
+                case 5 -> searchByVendor();
+                case 0 -> {
+                    LedgerApp.showLedger();
+                    done = true;
+                }
+                default -> System.out.println("\nINVALID OPTION... TRY AGAIN");
+
+            }
+        }
+    }
+    // display only month to date transactions in a report
+    private static void monthToDate() {
+        LocalDate today = LocalDate.now();
+        System.out.println("\n------------------------------------MONTH TO DATE REPORT--------------------------------------\n");
+        printHeader();
+        for (TransactionsInfo i : transactions) {
+            LocalDate transactionDate = i.getDate();
+            if (transactionDate.getMonth() == today.getMonth() && transactionDate.getYear() == today.getYear()) {
+                System.out.printf("%-15s %-15s %-25s %-20s %15s \n", i.getDate(), i.getTime(), i.getVendor(), i.getDescription(), i.getAmount());
+            }
+        }
+    }
+    //displays year to date transactions in a report
+    private static void yearToDate() {
+        LocalDate today = LocalDate.now();
+        System.out.println("\n----------------------------------YEAR TO DATE REPORT-----------------------------------------\n");
+        printHeader();
+        for (TransactionsInfo i : transactions) {
+            LocalDate transactionDate = i.getDate();
+            if (transactionDate.getYear() == today.getYear()) {
+                System.out.printf("%-15s %-15s %-25s %-20s %15s \n", i.getDate(), i.getTime(), i.getVendor(), i.getDescription(), i.getAmount());
+            }
+        }
+    }
+    //display previous year transactions in a report
+    private static void previousYear() {
+        LocalDate today = LocalDate.now();
+        int previousYear = today.minusYears(1).getYear();
+        System.out.println("\n---------------------------------PREVIOUS YEAR REPORT-----------------------------------------\n");
+        printHeader();
+        for (TransactionsInfo i : transactions) {
+            LocalDate transactionDate = i.getDate();
+            if (transactionDate.getYear() == previousYear) {
+                System.out.printf("%-15s %-15s %-25s %-20s %15s \n", i.getDate(), i.getTime(), i.getVendor(), i.getDescription(), i.getAmount());
+
+            }
+        }
+    }
+    // displays previous month transactions in a report
+    private static void previousMonth() {
+        LocalDate today = LocalDate.now();
+        int previousMonth = today.minusMonths(1).getMonthValue();
+        System.out.println("\n------------------------------------PREVIOUS MONTH REPORT--------------------------------------\n");
+        printHeader();
+        for (TransactionsInfo i : transactions) {
+            LocalDate transactionDate = i.getDate();
+            if (transactionDate.getMonthValue() == previousMonth) {
+                System.out.printf("%-15s %-15s %-25s %-20s %15s \n", i.getDate(), i.getTime(), i.getVendor(), i.getDescription(), i.getAmount());
+            }
+        }
+    }
+    // displays transactions filtered by the vendor name user desires
+    private static void searchByVendor() {
+        System.out.print("ENTER VENDOR NAME: ");
+        String vendorName = consoleInput.nextLine();
+        System.out.print("\n-----------------------------------TRANSACTIONS FOR: " + vendorName + "-------------------------------------\n\n");
+        printHeader();
+
+        for (TransactionsInfo i : transactions) {
+            if (i.getVendor().equalsIgnoreCase(vendorName)) {
+                System.out.printf("%-15s %-15s %-25s %-20s %15s \n", i.getDate(), i.getTime(), i.getVendor(), i.getDescription(), i.getAmount());
+
+            }
+        }
+    }
+    //header used throughout the class and other classes
+    public static void printHeader() {
+        System.out.printf("%-15s %-15s %-25s %-20s %15s \n", "DATE", "TIME", "DESCRIPTION", "VENDOR", "AMOUNT");
+        System.out.println("..............................................................................................");
+    }
+}
---
+++ src/main/resources/readme.md
@@ -0,0 +1,30 @@
+# Accounting Ledger 
+***Accounting Ledger Application***  allows user  to input and view accounting transactions.
+Also, user will be able to create reports based on different date ranges.
+
+## Features:
+
+* Able to add deposits
+* Able to make payments
+* Able to view ledger transactions
+* Able to create reports based on ranges
+    - Month To Date
+    - Year To Date
+    - Previous Month
+    - Previous Year
+    - Search by Vendor Name
+
+## Application Screen Images
+### Home Screen:
+![img.png](img.png)
+### Ledger Screen:
+![img_1.png](img_1.png)
+### List of Transactions:
+![img_2.png](img_2.png)
+### Reports Screen:
+![img_3.png](img_3.png)
+### Example of Report:
+![img_4.png](img_4.png)
+
+### Interesting Piece of Code:
+![img_5.png](img_5.png)
